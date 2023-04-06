114603a1cc0b567fae1c53d663487d6fef43b45c
Jan0770 <jo.otten2@web.de>
2023-04-06 15:47:12 +0200

    Included batch write to DynamoDB

--- RickMortyLocalTest.py
+++ RickMortyLocalTest.py
@@ -1,36 +1,32 @@
-import json
 import requests
-import boto3
 
-dynamodb = boto3.client('dynamodb')
-
-def lambda_handler(event, context):
+def getCharacters():
     characters = []
     
-    try:
+
 ######## GET Rick&Morty Characters
         
-        for i in range(1,4):
-            url = f"https://rickandmortyapi.com/api/character/?page={i}"    
-            response = requests.get(url)
-            result = response.json()
+    for i in range(1,4):
+        url = f"https://rickandmortyapi.com/api/character/?page={i}"    
+        response = requests.get(url)
+        result = response.json()
             
-            for j in range(20):
-                processedResults = {
-                    "ID": str(result["results"][j]["id"]),
-                    "Name": result["results"][j]["name"],
-                    "Species": result["results"][j]["species"],
-                    "Status": result["results"][j]["status"],
-                    "Origin": result["results"][j]["origin"]["name"],
-                    "Image": result["results"][j]["image"],
-                }
-                characters.append(processedResults)
+        for j in range(20):
+            processedResults = {
+                "ID": str(result["results"][j]["id"]),
+                "Name": result["results"][j]["name"],
+                "Species": result["results"][j]["species"],
+                "Status": result["results"][j]["status"],
+                "Origin": result["results"][j]["origin"]["name"],
+                "Image": result["results"][j]["image"],
+            }
+            characters.append(processedResults)
         
+    print(characters)    
         
 ######## DynamoDB
-        for i in range(60):
-            dynamodb.batch_write_item(
-                RequestItems={
+    for i in range(60):
+        RequestItems = {
                     'RickMortyAPITable': [
                         {
                             'PutRequest': {
@@ -58,13 +54,7 @@ def lambda_handler(event, context):
                         },
                     ],
                 },
-            )
-        
-
-        return characters    
-      
             
-    except Exception as e:
-        print(e)
-        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
-        raise e
+    print(RequestItems)
+
+getCharacters()
