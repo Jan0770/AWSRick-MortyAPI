e3ba1b590ed6dcbf73d3af998621c61f85ff050a
Jan0770 <jo.otten2@web.de>
2023-04-06 15:49:08 +0200

    Deleted unneccessary except statement

--- rickMortyApi.py
+++ rickMortyApi.py
@@ -60,11 +60,8 @@ def lambda_handler(event, context):
                 },
             )
         
-
         return characters    
       
-            
     except Exception as e:
         print(e)
-        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
         raise e
